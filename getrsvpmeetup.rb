1834198390e140aa6d2c5bb745da7e6d57c6b271
Brian Gupta <brian.gupta@brandorr.com>
2013-08-08 13:54:46 -0400

    Create getrsvpmeetup.rb

---
+++ getrsvpmeetup.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'json'
+require 'uri'
+require 'rest_client'
+require 'yaml'
+
+config = YAML.load(File.read("getrsvpmeetup.yml"))
+
+groupurlname = config["groupurlname"]
+apikey = config["apikey"]
+
+response = RestClient.get "https://api.meetup.com/2/events?key=" +
+ apikey + "&group_urlname=" + groupurlname + "&page=1&only=id"
+eventid = JSON.parse(response)["results"][0]["id"]
+
+response = RestClient.get "https://api.meetup.com/2/rsvps?key=" +
+ apikey + "&sign=true&event_id=" + eventid + "&page=400&rsvp=yes&order=name"
+
+JSON.parse(response)["results"].each { |x| puts x["member"]["name"] }
