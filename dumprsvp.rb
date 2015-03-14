4fc740694d9537d7a420dede49ebfc091c8aceb9
Brian Gupta <brian.gupta@brandorr.com>
2015-03-14 03:55:47 -0400

    dumprsvp.sh now includes seperated LastName and FirstName nested keys.

--- dumprsvp.rb
+++ dumprsvp.rb
@@ -56,24 +56,13 @@ $attendees = []
 end
 
 def isarealname (x)
-#   expr..
-#puts x["member"]["member_id"]
-#puts JSON.pretty_generate(x.to_json).to_s
-#puts JSON.pretty_generate(x["member"])
-#puts JSON.pretty_generate(x["answers"])
   if x["member"]["name"] == x["answers"][0]
     then 
       isreal = true
   else if x["answers"][0].empty?
     then
       isreal = true
-#      puts "No answer"
-#      puts "MeetupName: " + x["member"]["name"].to_s
-#      puts "Member_ID: " + x["member"]["member_id"].to_s + " exists"
-#      puts "Answer: " + x["answers"][0].to_s 
-#puts "They don't match, and they answered the RSVP question"
-#puts x["member"]["name"].to_json
-  else #puts x["answers"][0].to_json
+  else
     puts JSON.pretty_generate(x['member']['member_id'].to_s => { 'MeetupName' => x['member']['name'].to_s, 'Member_ID' => x['member']['member_id'].to_s, 'RealName' => x['answers'][0].to_s })
   end
   return isreal
@@ -85,14 +74,11 @@ $attendeehash = {}
 $attendees.each { |x|
   k = x['member']['member_id'].to_s
   if $memberhash.has_key?(k)
-    then $attendeehash[k] = { 'MeetupName' => x['member']['name'].to_s, 'Member_ID' => x['member']['member_id'].to_s, 'RealName' => $memberhash[k]['RealName'].to_s }
-#      if isarealname(x)
-#        then $memberhash[k] = { 'MeetupName' => x['member']['name'].to_s, 'Member_ID' => x['member']['member_id'].to_s, 'RealName' => x['member']['name'].to_s }
-#      end
-#    else puts x['member']['member_id'].to_s
+    then
+      $attendeehash[k] = { 'MeetupName' => x['member']['name'].to_s, 'Member_ID' => x['member']['member_id'].to_s, 'RealName' => $memberhash[k]['RealName'].to_s }
+      $attendeehash[k]['LastName'] = $attendeehash[k]['RealName'].split.last.to_str
+      $attendeehash[k]['FirstName'] = $attendeehash[k]['RealName'].gsub($attendeehash[k]['LastName'],'').rstrip
   end
 }
 
-#File.open(datafile, 'w') do |file|
-  puts JSON.pretty_generate($attendeehash)
-#end
+puts JSON.pretty_generate($attendeehash)
