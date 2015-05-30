d5e3c81ce6ff25155a2c95a3b58c15a267f66eb1
Brian Gupta <brian.gupta@brandorr.com>
2015-05-30 10:19:57 -0400

    Removed cruft newnames.rb

--- newnames.rb
+++ newnames.rb
@@ -15,25 +15,15 @@ if File.file?(datafile)
 else $memberhash = {}
 end
 
-#cleanednamesfile = ENV['HOME'] + '/Downloads/nylug-out.json'
-#orignamesfile = ENV['HOME'] + 'nylug-rsvp-april15-1.json'
-#orignamesfile = 'nylug-rsvp-april15-1.json'
-#orignamesfile = 'NYLUGMay14RSVP.json'
 orignamesfile = ARGV[0]
-
-#newnamesfile = ENV['HOME'] + 'nylug-rsvp-april15-3.json'
-#newnamesfile = 'nylug-rsvp-april15-3.json'
-#newnamesfile = 'NYLUGMay14RSVP-2.json'
 newnamesfile = ARGV[1]
 
 if File.file?(orignamesfile)
   then $orignameshash = JSON.load(File.read(orignamesfile))
-#else $orignameshash = {}
 end
 
 if File.file?(newnamesfile)
   then $newnameshash = JSON.load(File.read(newnamesfile))
-#else $orignameshash = {}
 end
 
 $newnameshash.each { |x|
@@ -44,8 +34,4 @@ $newnameshash.each { |x|
   end
 }
 
-#File.open(datafile, 'w') do |file|
-#  file.write JSON.pretty_generate($memberhash)
-#end
 puts JSON.pretty_generate($newnameshash)
-
