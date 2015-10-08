1576905ab432c3006d41644fc0d24f0668f93245
Brian Gupta <brian.gupta@brandorr.com>
2015-10-08 09:51:52 -0400

    Updated dumpsuspects to print meetupname

--- dumpsuspects.rb
+++ dumpsuspects.rb
@@ -84,7 +84,7 @@ $attendees.each { |x|
   k = x['member']['member_id'].to_s
   if $memberhash.has_key?(k)
     then if $memberhash[k]['Status'] == 'Suspect'
-      then puts $memberhash[k]['RealName'].to_s + "\t\t\t " + $memberhash[k]['Member_ID'].to_s + "\t " + "Name:___________________________________________".to_s
+      then puts $memberhash[k]['MeetupName'].to_s + "\t\t\t " + $memberhash[k]['Member_ID'].to_s + "\t " + "Name:___________________________________________".to_s
 #      then printf("%-10s %10s",$memberhash[k]['RealName'].to_s,  $memberhash[k]['Member_ID'].to_s,  "Name:_____________________________________________________".to_s)
     end 
 #      if isarealname(x)
